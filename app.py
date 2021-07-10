4c2fd0f287fa1e760fd9f65757e43b75fc55e64c
Dheep Datascience <48237958+dheepdatascigit@users.noreply.github.com>
2021-07-10 15:57:53 +1000

    env set in app service
    Print the environment variable setup via Azure app service configuration

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Brave new world"
+    return "Welcome to Brave new world {}".format(os.environ['AUTHOR_NAME'])
